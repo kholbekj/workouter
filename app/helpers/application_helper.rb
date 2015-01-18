c43e037f5f7a43e876c13827362670b3e976d50d
Kasper Holbek Jensen <khj@autobutler.dk>
2015-01-18 14:58:59 +0100

    use the external path for something.........

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   ASSET_PREFIX = "https://s3.eu-central-1.amazonaws.com/workouter-assets/"
   def external_image(path)
-    "images/#{path}"
+    "#{ASSET_PREFIX}images/#{path}"
   end
 end
